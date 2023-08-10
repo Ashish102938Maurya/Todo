dcc926098be59dc17208cda2792dfb83d6461534
Ashish Maurya <am7620613@gmail.com>
2023-08-10 23:03:00 +0530

    Files Added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import TODO from './components/TODO';
+import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <div class="container-sm">
+      <TODO/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Input.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function Input() {
+    const [Value, setValue] = useState("");
+    const [Fvalue, setFvalue] = useState([]);
+    const [Toggle, setToggle] = useState(false);
+    const [editedVal, seteditedVal] = useState(null);
+    const notify = () => toast("😂Empty Text😂");
+
+    const Add = () => {
+        if (!Value) {
+            notify();
+
+        }
+        else if (Value && Toggle) {   // if there is edit icon and some value in input tag
+            setFvalue(
+                Fvalue.map((ele) => {
+                    if (ele.id === editedVal) {
+                        return { ...ele, name: Value }
+                    }
+                    return ele;
+                })
+            )
+            setValue("");
+            setToggle(false);
+            seteditedVal(null);
+        }
+        else {                        // if there is plus icon and some value in the input tag
+            const addVal = { id: new Date().getTime().toString(), name: Value }
+            setFvalue([...Fvalue, addVal]);
+            setValue("");
+        }
+    }
+
+    const deleteItem = (delId) => {
+        const filterArray = Fvalue.filter((ele) => { return ele.id !== delId })
+        setFvalue(filterArray);
+    }
+
+    const editItem = (editId) => {
+        const filterArray = Fvalue.find((ele) => {
+            return ele.id === editId;
+        })
+        setValue(filterArray.name);
+        setToggle(true);
+        seteditedVal(editId)
+    }
+
+    return (
+        <>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="✍️ Write Here" aria-label="Recipient's username" aria-describedby="button-addon2" value={Value} onChange={(eve) => { setValue(eve.target.value) }} />
+                <button class="btn btn-outline-success" type="button" id="button-addon2" onClick={Add}> {Toggle ? (<i class="fa-solid fa-pen-to-square" ></i>) : (<i class="fa-solid fa-plus"></i>)} </button>
+            </div>
+
+            <ul className="list-group">
+                {Fvalue.map((ele) => (
+                    <li class="list-group-item mb-2 d-flex justify-content-between align-items-center" >
+                        {ele.name}
+                        <div>
+                            <button class="me-1 mb-1 " onClick={() => deleteItem(ele.id)} ><i class="fa-solid fa-trash"></i></button>
+                            <button onClick={() => editItem(ele.id)}><i class="fa-solid fa-pen-to-square" ></i></button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+        </>
+    )
+}
