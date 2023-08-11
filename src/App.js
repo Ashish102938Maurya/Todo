8e7f6d1fc1f0cc2babbb1408768914c070e6ecd0
Ashish  Maurya <118504361+Ashish48Maurya@users.noreply.github.com>
2023-08-11 23:48:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import TODO from './components/TODO';
 import './App.css';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
